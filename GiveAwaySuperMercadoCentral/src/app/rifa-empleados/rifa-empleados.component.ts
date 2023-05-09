093e4549c29583bc7cbca6a6ec1d650d060dc1cf
Rafael Gomez <rafaelantoniogomezhilario@gmail.com>
2023-05-09 12:31:39 -0400

    update
    update

--- GiveAwaySuperMercadoCentral/src/app/rifa-empleados/rifa-empleados.component.ts
+++ GiveAwaySuperMercadoCentral/src/app/rifa-empleados/rifa-empleados.component.ts
@@ -23,21 +23,21 @@ export class RifaEmpleadosComponent implements OnInit {
   chosenElements: any = []
   excelFinal: any = []
   idPremio: any;
-  preios: string= '';
+  preios: string = '';
   archivoSubido = false;
   archivoCargado: any;
   ganadoresHabilitado: boolean = false;
 
 
-  constructor(private route: ActivatedRoute, private Sqlservicio: ServiciosService, public router: Router,private http: HttpClient) {
+  constructor(private route: ActivatedRoute, private Sqlservicio: ServiciosService, public router: Router, private http: HttpClient) {
     this.premios()
     this.verificarPremiosEnLocalStorage();
   }
 
   ngOnInit(): void {
 
-}
-verificarPremiosEnLocalStorage() {
+  }
+  verificarPremiosEnLocalStorage() {
     console.log("hola")
     const premios = localStorage.getItem('Premios');
     if (!premios) {
@@ -48,186 +48,188 @@ verificarPremiosEnLocalStorage() {
       this.router.navigate(['/']);
     }
   }
-premios() {
-  this.verificarPremiosEnLocalStorage()
-  this.arrayPremios = JSON.parse(localStorage.getItem("Premios")!)
-  const ganador = this.arrayPremios[Math.floor(Math.random() * this.arrayPremios.length)];
+  premios() {
+    this.verificarPremiosEnLocalStorage()
+    this.arrayPremios = JSON.parse(localStorage.getItem("Premios")!)
+    const ganador = this.arrayPremios[Math.floor(Math.random() * this.arrayPremios.length)];
 
-}
-
-selectPremio(e: Event) {
-  let Id: any = ((e.target as HTMLInputElement).value);
-  this.idPremio = String(Id)
-  this.preios =  String(Id);
-  console.log(this.idPremio)
-}
-sorteoGiveAway(item: any) {
-  // Swal.showLoading(Swal.getDenyButton());
-  console.log(this.excelFinal)
-  this.chosenElements = []
-  const cedulas = new Set()
-  console.log(this.chosenElements)
-  while (this.chosenElements.length < item) {
-    console.log(this.chosenElements)
-     const randomIndex = Math.floor(Math.random() * this.excelFinal.length)
-     const randomElement = this.excelFinal[randomIndex]
-     if (!cedulas.has(randomElement.Cedula)) {
-         this.chosenElements.push(randomElement)
-         cedulas.add(randomElement.Cedula)
-     }
   }
 
-  this.excelFinal = this.excelFinal.filter((objeto: any) => !this.chosenElements.some((o: any) => o.Cedula === objeto.Cedula));
+  selectPremio(e: Event) {
+    let Id: any = ((e.target as HTMLInputElement).value);
+    this.idPremio = String(Id)
+    this.preios = String(Id);
+    console.log(this.idPremio)
+  }
+  sorteoGiveAway(item: any) {
+    // Swal.showLoading(Swal.getDenyButton());
+    console.log(this.excelFinal)
+    this.chosenElements = []
+    const cedulas = new Set()
+    console.log(this.chosenElements)
+    while (this.chosenElements.length < item) {
+      console.log(this.chosenElements)
+      const randomIndex = Math.floor(Math.random() * this.excelFinal.length)
+      const randomElement = this.excelFinal[randomIndex]
+      if (!cedulas.has(randomElement.Cedula)) {
+        this.chosenElements.push(randomElement)
+        cedulas.add(randomElement.Cedula)
+      }
+    }
 
-  for (let i = 0; i < this.chosenElements.length; i++) {
-    this.chosenElements[i].premio = this.idPremio
+    this.excelFinal = this.excelFinal.filter((objeto: any) => !this.chosenElements.some((o: any) => o.Cedula === objeto.Cedula));
 
-    this.Sqlservicio.ganadores(this.chosenElements[i]).subscribe((data: any) => {
-    })
+    for (let i = 0; i < this.chosenElements.length; i++) {
+      this.chosenElements[i].premio = this.idPremio
+
+      this.Sqlservicio.ganadores(this.chosenElements[i]).subscribe((data: any) => {
+      })
 
       this.proximoGanador();
       // Swal.showLoading(Swal.getDenyButton());
 
-  }
-  console.log(this.chosenElements)
-}
-sorteo(item: any) {
-  // Swal.showLoading(Swal.getDenyButton());
-  console.log(this.excelFinal)
-  this.chosenElements = []
-  const codigos = new Set()
-  console.log(this.chosenElements)
-  while (this.chosenElements.length < item) {
+    }
     console.log(this.chosenElements)
-     const randomIndex = Math.floor(Math.random() * this.excelFinal.length)
-     const randomElement = this.excelFinal[randomIndex]
-     if (!codigos.has(randomElement.codigoInterno)) {
-         this.chosenElements.push(randomElement)
-         codigos.add(randomElement.codigoInterno)
-     }
-
   }
+  sorteo(item: any) {
+    // Swal.showLoading(Swal.getDenyButton());
+    console.log(this.excelFinal)
+    this.chosenElements = []
+    const codigos = new Set()
+    console.log(this.chosenElements)
+    while (this.chosenElements.length < item) {
+      console.log(this.chosenElements)
+      const randomIndex = Math.floor(Math.random() * this.excelFinal.length)
+      const randomElement = this.excelFinal[randomIndex]
+      if (!codigos.has(randomElement.codigoInterno)) {
+        this.chosenElements.push(randomElement)
+        codigos.add(randomElement.codigoInterno)
+      }
 
-  this.excelFinal = this.excelFinal.filter((objeto: any) => !this.chosenElements.some((o: any) => o.codigoInterno === objeto.codigoInterno));
+    }
 
-  for (let i = 0; i < this.chosenElements.length; i++) {
-    this.chosenElements[i].premio = this.idPremio
-    this.Sqlservicio.ganadoresInternos(this.chosenElements[i]).subscribe((data: any) => {
-      console.log(data)
-    })
+    this.excelFinal = this.excelFinal.filter((objeto: any) => !this.chosenElements.some((o: any) => o.codigoInterno === objeto.codigoInterno));
+
+    for (let i = 0; i < this.chosenElements.length; i++) {
+      this.chosenElements[i].premio = this.idPremio
+      this.Sqlservicio.ganadoresInternos(this.chosenElements[i]).subscribe((data: any) => {
+        console.log(data)
+      })
 
       this.proximoGanador();
       // Swal.showLoading(Swal.getDenyButton());
 
+    }
+    console.log(this.chosenElements)
   }
-  console.log(this.chosenElements)
-}
-ReadExcel(event: any) {
+  ReadExcel(event: any) {
 
-  let file = event.target.files[0];
-  let fileReader = new FileReader();
-  fileReader.readAsBinaryString(file);
-  fileReader.onload = (e) => {
-    var workBook = XLSX.read(fileReader.result, { type: 'binary', cellText: true });
-    var sheetNames = workBook.SheetNames;
+    let file = event.target.files[0];
+    let fileReader = new FileReader();
+    fileReader.readAsBinaryString(file);
+    fileReader.onload = (e) => {
+      var workBook = XLSX.read(fileReader.result, { type: 'binary', cellText: true });
+      var sheetNames = workBook.SheetNames;
 
-    this.ExcelData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]])
+      this.ExcelData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]])
 
-    this.ExcelData.forEach((element: any) => {
-      let datoFinal =
-      {
-        Boleta: element.Boleta,
-        Cedula: String(element.Cedula),
-        Factura: element.Factura,
-        Fecha: element.Fecha,
-        Nombre: element.Nombre,
-        Telefono: element.Telefono,
-        premio: this.idPremio,
-        No1: 0
-      }
-      this.excelFinal.push(datoFinal)
-      console.log(this.excelFinal)
-    });
+      this.ExcelData.forEach((element: any) => {
+        let datoFinal =
+        {
+          Boleta: element.Boleta,
+          Cedula: String(element.Cedula),
+          Factura: element.Factura,
+          Fecha: element.Fecha,
+          Nombre: element.Nombre,
+          Telefono: element.Telefono,
+          premio: this.idPremio,
+          No1: 0
+        }
+        this.excelFinal.push(datoFinal)
+        console.log(this.excelFinal)
+      });
+    }
   }
-}
 
-ReadExcelInterno(event: any) {
-  let file = event.target.files[0];
-  let fileReader = new FileReader();
-  fileReader.readAsBinaryString(file);
-  fileReader.onload = (e) => {
-    var workBook = XLSX.read(fileReader.result, { type: 'binary', cellText: true });
-    var sheetNames = workBook.SheetNames;
+  ReadExcelInterno(event: any) {
+    let file = event.target.files[0];
+    let fileReader = new FileReader();
+    fileReader.readAsBinaryString(file);
+    fileReader.onload = (e) => {
+      var workBook = XLSX.read(fileReader.result, { type: 'binary', cellText: true });
+      var sheetNames = workBook.SheetNames;
 
-    this.ExcelData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]])
+      this.ExcelData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]])
 
-    this.ExcelData.forEach((element: any) => {
-      let datoFinal =
-      {
-        Fecha: element.Fecha,
-        nombre: element.nombre,
-        premio: this.idPremio,
-        codigoInterno: element.codigoInterno
-      }
-      this.excelFinal.push(datoFinal)
-      console.log(this.excelFinal)
-    });
+      this.ExcelData.forEach((element: any) => {
+        let datoFinal =
+        {
+          Fecha: element.Fecha,
+          nombre: element.nombre,
+          premio: this.idPremio,
+          codigoInterno: element.codigoInterno
+        }
+        this.excelFinal.push(datoFinal)
+        console.log(this.excelFinal)
+      });
 
-    // Set archivoCargado and ganadoresHabilitado to true when file is loaded
-    this.archivoSubido = true;
-    this.ganadoresHabilitado = true;
+      // Set archivoCargado and ganadoresHabilitado to true when file is loaded
+      this.archivoSubido = true;
+      this.ganadoresHabilitado = true;
 
+    }
   }
-}
 
-// Empleado Generador
-proximoGanador() {
-  if (!this.archivoSubido) {
-    Swal.fire({
+  // Empleado Generador
+  proximoGanador() {
+    if (!this.archivoSubido) {
+      Swal.fire({
         icon: 'warning',
         title: 'Archivo no cargado',
         html: 'Por favor, cargue un Documento antes de continuar.<br>Para hacerlo, presione la TUERCA.',
         confirmButtonText: 'Aceptar'
-    });
-    return;
-  }
-  setTimeout(() => {
-    let indexGanador = 0;
-    let self = this;
-    function mostrarAlerta(i: any) {
-      if (i < self.chosenElements.length) {
-        setTimeout(() => {
-          Swal.showLoading();
+      });
+      return;
+    }
+    setTimeout(() => {
+      let indexGanador = 0;
+      let self = this;
+      function mostrarAlerta(i: any) {
+        if (i < self.chosenElements.length) {
+          setTimeout(() => {
+            Swal.showLoading();
 
-        Swal.fire({
-          title: '<h1>' + '<b>SUPERMERCADO CENTRAL TE PREMIA!!</b>' + '</h1>' +'<br>'+ '<h1>' + '<b>GANADOR  #</b>' + (i + 1) +'<br>'+'<br>'+'<b>Premio' +'<br>'+'<h1>'+self.chosenElements[i].premio +'</h1>'+'</h1>' +'<br>',
-          width: 1200,
-          padding: '7em',
-          html: '<h1>' + '<b>Nombre:</b>' + '</h1>' + '<h1>' + self.chosenElements[i].nombre + '</h1>' + '<br>' +'<h1>' + self.chosenElements[i].codigoInterno  + '</h1>',
-          color: '#218e27',
-          background: '#fff url(assets/confetti.gif)',
-          backdrop: `
+            Swal.fire({
+              title: '<h1>' + '<b>SUPERMERCADO CENTRAL TE PREMIA!!</b>' + '</h1>' + '<br>' + '<h1>' + '<b>GANADOR  #</b>' + (i + 1) + '<br>' + '<br>' + '<b>Premio' + '<br>' + '<h1>' + self.chosenElements[i].premio + '</h1>' + '</h1>' + '<br>',
+              width: 1200,
+              padding: '7em',
+              html: '<h1>' + '<b>Nombre:</b>' + '</h1>' + '<h1>' + self.chosenElements[i].nombre + '</h1>' + '<br>' + '<h1>' + self.chosenElements[i].codigoInterno + '</h1>',
+              color: '#218e27',
+              background: '#fff url(assets/confetti.gif)',
+              backdrop: `
               rgba(0,0,123,0.4)
                 url("https://i.gifer.com/W9k1.gif")
               `,
-          confirmButtonText: '<h3>' + 'Siguiente Ganador' +'<h3>',
-        }).then(() => {
-          mostrarAlerta(i + 1)
-        })
-      },1000);
+              confirmButtonText: '<h3>' + 'Siguiente Ganador' + '<h3>',
+            }).then(() => {
+              mostrarAlerta(i + 1)
+            })
+          }, 1000);
+        }
       }
+      // Swal.showLoading(Swal.getDenyButton());
+
+      mostrarAlerta(indexGanador)
+    }, 1000);
+  }
+
+  ir(e: any) {
+    if (e == 1) {
+      this.router.navigateByUrl('/menu')
     }
-    // Swal.showLoading(Swal.getDenyButton());
-
-    mostrarAlerta(indexGanador)
-  }, 1000);
-}
-
-ir(e:any){
-  if(e==1){
-    this.router.navigateByUrl('/menu')
   }
 }
-}
+
+
 
 
